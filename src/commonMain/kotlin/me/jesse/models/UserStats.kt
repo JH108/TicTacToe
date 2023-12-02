1e5c44cf693696ed531f9c0550ac6b3b5aee8ddb
Jesse Hill <jessehill108@gmail.com>
2023-12-02 13:40:18 -0600

    Update the UI

--- src/commonMain/kotlin/me/jesse/models/UserStats.kt
+++ src/commonMain/kotlin/me/jesse/models/UserStats.kt
@@ -14,6 +14,6 @@ data class UserStats(
     val totalDraws: Long?
 ) {
     val totalWins = (totalXWins ?: 0) + (totalOWins ?: 0)
-    val winPercent = if (totalGames > 0) totalWins.toDouble() / totalGames.toDouble() else 0.0
+    private val winPercent = if (totalGames > 0) totalWins.toDouble() / totalGames.toDouble() else 0.0
     val winPercentString = "${(winPercent * 100).toInt()}%"
 }
