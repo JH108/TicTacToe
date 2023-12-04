3e1ecebc73cfa142187f40db42b811812c6031c4
Jesse Hill <jessehill108@gmail.com>
2023-12-03 18:54:34 -0600

    Put the base url to the deployed url...hardcode...

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "me.jesse"
-version = "1.0-SNAPSHOT"
+version = "1.2-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- src/jsMain/kotlin/Client.kt
+++ src/jsMain/kotlin/Client.kt
@@ -8,7 +8,7 @@ import react.dom.client.createRoot
 
 object ClientConfiguration {
     // TODO: Change this based on the build type (dev, prod, etc)
-    val baseUrl = "http://localhost:8080"
+    val baseUrl = "http://tictactoe.jessehill.me"
     val apiUrl = "$baseUrl/api"
 }
 
