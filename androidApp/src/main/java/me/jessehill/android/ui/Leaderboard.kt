b07517e7bd636e4570c40909744747e6b3f276b4
Jesse Hill <jessehill108@gmail.com>
2024-03-19 10:37:18 -0500

    Some leader board changes

--- androidApp/src/main/java/me/jessehill/android/ui/Leaderboard.kt
+++ androidApp/src/main/java/me/jessehill/android/ui/Leaderboard.kt
@@ -1,10 +1,44 @@
 package me.jessehill.android.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import me.jessehill.android.TicTacToeState
 
 @Composable
-fun Leaderboard(state: TicTacToeState) {
-    Text("Hi")
+fun Leaderboard(
+    modifier: Modifier = Modifier
+    state: TicTacToeState
+) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        state.leaderboard.forEach { (user, userStats) ->
+            Card {
+                Text(
+                    text = "Total Games: ${userStats.totalGames}"
+                )
+                Row {
+                    Text(
+                        text = "Wins: ${userStats.totalWins}"
+                    )
+                    Text(
+                        text = "Draws: ${userStats.totalDraws}"
+                    )
+                }
+                Row {
+                    Text(
+                        text = "Wins as X: ${userStats.totalXWins}"
+                    )
+                    Text(
+                        text = "Wins as O: ${userStats.totalOWins}"
+                    )
+                }
+            }
+        }
+    }
 }
